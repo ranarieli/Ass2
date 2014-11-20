361148e5c2264082eeb4fec8a112331a114b85de
Henmev <Henmev@gmail.com>
2014-11-20 17:05:23 +0200

    Create Junction.cpp

---
+++ src/Junction.cpp
@@ -0,0 +1,23 @@
+/*
+ * Junction.cpp
+ *
+ *  Created on: Nov 12, 2014
+ *      Author: ran
+ */
+
+#include "../include/Junction.h"
+
+Junction::Junction() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Junction::Junction(const string id):_id(id) {
+	// TODO Auto-generated constructor stub
+
+}
+
+Junction::~Junction() {
+	// TODO Auto-generated destructor stub
+}
+
