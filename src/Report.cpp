cd0ebf176ffda34f599efb11d051580180ae4f37
Henmev <Henmev@gmail.com>
2014-11-20 17:05:57 +0200

    Create Report.cpp

---
+++ src/Report.cpp
@@ -0,0 +1,29 @@
+/*
+ * Report.cpp
+ *
+ *  Created on: Nov 12, 2014
+ *      Author: ran
+ */
+
+#include "../include/Report.h"
+
+Report::Report() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Report::~Report() {
+	// TODO Auto-generated destructor stub
+}
+
+class CarReport : public Report {
+
+};
+
+class RoadReport : public Report {
+
+};
+
+class JunctionReport : public Report {
+
+};
