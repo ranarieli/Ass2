2ad2e601d1b2d209ea2003df4a9c3404cd0321af
Henmev <Henmev@gmail.com>
2014-11-20 17:07:23 +0200

    Create CityDrivingCimulator.h

---
+++ include/CityDrivingCimulator.h
@@ -0,0 +1,17 @@
+/*
+ * CityDrivingCimulator.h
+ *
+ *  Created on: Nov 12, 2014
+ *      Author: ran
+ */
+
+#ifndef CITYDRIVINGCIMULATOR_H_
+#define CITYDRIVINGCIMULATOR_H_
+
+class CityDrivingCimulator {
+public:
+	CityDrivingCimulator();
+	virtual ~CityDrivingCimulator();
+};
+
+#endif /* CITYDRIVINGCIMULATOR_H_ */
