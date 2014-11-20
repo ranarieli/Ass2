fd291754648fec77043bb602d6b247358ff01567
Henmev <Henmev@gmail.com>
2014-11-20 17:04:23 +0200

    Create Car.cpp

---
+++ src/Car.cpp
@@ -0,0 +1,18 @@
+/*
+ * Car.cpp
+ *
+ *  Created on: Nov 12, 2014
+ *      Author: ran
+ */
+
+#include "../include/Car.h"
+
+Car::Car() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Car::~Car() {
+	// TODO Auto-generated destructor stub
+}
+
