30416429ff710b2855d45349e88c5f5e58f11877
Henmev <Henmev@gmail.com>
2014-11-20 17:04:50 +0200

    Create CityDrivingCimulator.cpp

---
+++ src/CityDrivingCimulator.cpp
@@ -0,0 +1,18 @@
+/*
+ * CityDrivingCimulator.cpp
+ *
+ *  Created on: Nov 12, 2014
+ *      Author: ran
+ */
+
+#include "../include/CityDrivingCimulator.h"
+
+CityDrivingCimulator::CityDrivingCimulator() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CityDrivingCimulator::~CityDrivingCimulator() {
+	// TODO Auto-generated destructor stub
+}
+
